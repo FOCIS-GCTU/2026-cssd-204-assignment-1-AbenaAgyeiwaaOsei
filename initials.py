99b3146e06f4ffa58e7ae8e522f9371d15991c67
AbenaAgyeiwaaOsei <2425404329@live.gctu.edu.gh>
2026-05-30 19:37:31 +0000

    Fix indentation and formatting in initials.py

--- initials.py
+++ initials.py
@@ -1,32 +1,32 @@
- # File: initials.py
- # Description: Print out my initials(my name)
- # Assignment: 2
- #
- # Name: Abena Agyeiwaa Osei
- # STUDENT ID: 2425404329
- # Email: 2425404329@live.gctu.edu.gh
- # Grader: Mr.Augustus Buckman
- #
- # On my honor,Abena Agyeiwaa Osei,this programming assignment is my own
- # work and I have not provided this code to any other student.
+# File: initials.py
+# Description: Print out my initials(my name)
+# Assignment: 2
+#
+# Name: Abena Agyeiwaa Osei
+# STUDENT ID: 2425404329
+# Email: 2425404329@live.gctu.edu.gh
+# Grader: Mr.Augustus Buckman
+#
+# On my honor,Abena Agyeiwaa Osei,this programming assignment is my own
+# work and I have not provided this code to any other student.
 
 
 def main():
-# Print the initials AAO.
-     print()
-         print("...AAO")
-         print()
-         print("...AAAAAAAAAAAA****...AAAAAAAAAAAA****...OOOOOOOOOOOO")
-         print("...AA........AA****...AA........AA****...OO........OO")
-         print("...AA........AA****...AA........AA****...OO........OO")
-         print("...AAAAAAAAAAAA****...AAAAAAAAAAAA****...OO........OO")
-         print("...AA........AA****...AA........AA****...OO........OO")
-         print("...AA........AA****...AA........AA****...OO........OO")
-         print("...AA........AA****...AA........AA****...OO........OO")
-         print("...AA........AA****...AA........AA****...OO........OO")
-         print("...AA........AA****...AA........AA****...OO........OO")
-         print("...AA........AA****...AA........AA****...OOOOOOOOOOOO")
-     print()
+ # Print the initials AAO.
+    print()
+    print("...AAO")
+    print()
+    print("...AAAAAAAAAAAA****...AAAAAAAAAAAA****...OOOOOOOOOOOO")
+    print("...AA........AA****...AA........AA****...OO........OO")
+    print("...AA........AA****...AA........AA****...OO........OO")
+    print("...AAAAAAAAAAAA****...AAAAAAAAAAAA****...OO........OO")
+    print("...AA........AA****...AA........AA****...OO........OO")
+    print("...AA........AA****...AA........AA****...OO........OO")
+    print("...AA........AA****...AA........AA****...OO........OO")
+    print("...AA........AA****...AA........AA****...OO........OO")
+    print("...AA........AA****...AA........AA****...OO........OO")
+    print("...AA........AA****...AA........AA****...OOOOOOOOOOOO")
+    print()
 
 
 main()
