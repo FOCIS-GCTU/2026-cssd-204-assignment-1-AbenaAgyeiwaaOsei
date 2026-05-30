89f4dd07a3f51455a1e2bc15b218db53a59d913c
AbenaAgyeiwaaOsei <2425404329@live.gctu.edu.gh>
2026-05-30 19:30:34 +0000

    Print Initials
    Implemented a function to print initials 'AAO' with decorative patterns.

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+ # File: initials.py
+ # Description: Print out my initials(my name)
+ # Assignment: 2
+ #
+ # Name: Abena Agyeiwaa Osei
+ # STUDENT ID: 2425404329
+ # Email: 2425404329@live.gctu.edu.gh
+ # Grader: Mr.Augustus Buckman
+ #
+ # On my honor,Abena Agyeiwaa Osei,this programming assignment is my own
+ # work and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
+# Print the initials AAO.
+     print()
+         print("...AAO")
+         print()
+         print("...AAAAAAAAAAAA****...AAAAAAAAAAAA****...OOOOOOOOOOOO")
+         print("...AA........AA****...AA........AA****...OO........OO")
+         print("...AA........AA****...AA........AA****...OO........OO")
+         print("...AAAAAAAAAAAA****...AAAAAAAAAAAA****...OO........OO")
+         print("...AA........AA****...AA........AA****...OO........OO")
+         print("...AA........AA****...AA........AA****...OO........OO")
+         print("...AA........AA****...AA........AA****...OO........OO")
+         print("...AA........AA****...AA........AA****...OO........OO")
+         print("...AA........AA****...AA........AA****...OO........OO")
+         print("...AA........AA****...AA........AA****...OOOOOOOOOOOO")
+     print()
 
 
 main()
